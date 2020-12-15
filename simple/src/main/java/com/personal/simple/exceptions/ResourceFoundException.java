835129272cf4a6a398a37831b1453bdf92378714
1like2learn <ndespained@gmail.com>
2020-12-15 11:07:58 -0800

    adds a couple exceptions

--- simple/src/main/java/com/personal/simple/exceptions/ResourceFoundException.java
+++ simple/src/main/java/com/personal/simple/exceptions/ResourceFoundException.java
@@ -2,7 +2,12 @@ package com.personal.simple.exceptions;
 
 public class ResourceFoundException extends RuntimeException {
     
-    public ResourceFoundException(String message){
+    /**
+     *
+     */
+    private static final long serialVersionUID = 3849617049190420159L;
+
+    public ResourceFoundException(String message) {
 
         super("Error from simple API " + message);
     }
